b65ddf59c0b51b2e463e1aa00c360f97f0a518dc
serdg0 <sergio.diazg92@gmail.com>
2020-03-09 12:42:10 -0600

    init

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest });
 
-// You can delete this file if you're not using it
+const projects = [
+    {}
+]
