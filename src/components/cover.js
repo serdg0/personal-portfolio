7e70c53db301677d85ef18171577c1f58b94d54b
serdg0 <sergio.diazg92@gmail.com>
2020-03-13 13:20:28 -0600

    Fix small screen overlap

--- src/components/cover.js
+++ src/components/cover.js
@@ -12,17 +12,15 @@ const Cover = ({ children, title, description }) => {
         <div style={{height: '100vh'}} id="up">
             {children}
             <div className="h-25" />
-            <h1 className="main-color mb-5 mt-5" style={fontSize}>
                 <Media queries={{ small: { maxWidth: 760 } }}>
                     {matches =>
                         matches.small ? (
-                            <Typical loop={1} steps={[title, 5000]} />
+                            <h1><Typical className="main-color mb-5 mt-5" loop={1} steps={[title, 5000]} /></h1>
                         ) : (
-                            <Typical loop={1} steps={['> ' + title, 5000]} />
+                            <h1 className="main-color mb-5 mt-5" style={fontSize}><Typical loop={1} steps={['> ' + title, 5000]} /></h1>
                             )
                     }
                 </Media>
-            </h1>
             <h5 className="text-color">{description}</h5>
             <Slides />
         </div>
